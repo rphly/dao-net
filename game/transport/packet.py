fa33f3443e8ea75a8778cd7ae01729d7fb2bf23d
rphly <raphaelyeejunkai@gmail.com>
2023-03-19 16:29:28 +0800

    add packet

---
+++ game/transport/packet.py
@@ -0,0 +1,46 @@
+from typing import Union
+from game.models.action import Action
+import json
+import time
+
+
+class Packet:
+    """
+    We enclose all data to be sent over the network in a packet.
+
+    Accepted data:
+    - Action
+    - SyncReq
+    - SyncAck
+    - SyncUpdate
+    - LobbyRegister
+    - LobbyLeave
+    - LobbyStart
+    - LobbySaveTracker
+    """
+    ACCEPTED_TYPES = Union[Action, dict]
+
+    def __init__(self, payload: ACCEPTED_TYPES):
+        self.data = payload.get_data()
+        self.player = payload.get_player()
+        self.payloadType = get_type(payload)
+        self.createdAt = int(time.time())
+
+    def json(self) -> str:
+        """Return a json representation of the packet."""
+        return json.dumps(dict(
+            data=self.data,
+            player=self.player,
+            payload_type=self.payloadType,
+            created_at=self.createdAt
+        ))
+
+    def __str__(self):
+        return f"Packet: {str(self.data)}"
+
+
+def get_type(p) -> str:
+    """Return the type of the payload."""
+    if isinstance(p, Action):
+        return "action"
+    return "unknown"
