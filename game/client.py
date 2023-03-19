bba22f1ed9153a13f59ff09374d6a5e44b84699a
fish-r <gyf8800@gmail.com>
2023-03-19 11:55:02 +0800

    feature: lobby state, end round state, added player attributes

--- game/client.py
+++ game/client.py
@@ -1,5 +1,6 @@
 from game.engine.core import Core
 from game.models import Player
+import config
 
 
 class Client():
@@ -10,21 +11,27 @@ class Client():
     def __init__(self, engine=Core(),):
         super().__init__()
         self._game_engine: Core = engine
+        self._state: str = "LOBBY"
+
         self._players: dict[str, Player] = {}
-        self._state: str = "lobby"
+        self._inputs: dict[str, str] = {}
+
+        self._current_chairs: int = config.NUM_CHAIRS
 
     def state(self):
         return self._state
 
     def trigger_handler(self, state):
-        if state == "lobby":
+        if state == "LOBBY":
             self._lobby()
-        if state == "init":
+        if state == "INIT":
             self._init()
         elif state == "ready":
             self._ready()
-        elif state == "play":
+        elif state == "PLAY":
             return self._play()
+        elif state == "END_ROUND":
+            self._
         elif state == "end":
             return self._end()
 
@@ -32,15 +39,15 @@ class Client():
         # check for players
         # if not enough players:
         # call next immediately
-        # else when all done change to init and call next
-        ...
+        players = self.get_players
+        if len(players) == config.NUM_PLAYERS:
+            self._set_state("INIT")
+        self.next()
 
     def _init(self):
-        # if not yet sync:
-        # init game clock sync
-        # if not sync done:
-        # call next immediately
-        # else when all done change to ready and call next
+        # start counting down
+        # do all the network stuff here
+
         ...
 
     def _ready(self):
@@ -51,11 +58,21 @@ class Client():
         # next()
         ...
 
-    def _start(self):
+    def _play(self):
+        # wait for current player keypress
+        # verify if keypress is qwerty or overlapped
         ...
 
-    def _start(self):
-        ...
+    def _end_round(self):
+        # if player failed to input/ find a chair, kick him
+        for player in self._players.keys:
+            if player not in self._inputs:
+                player.alive = False
+
+        # reduce chair by 1
+        self._reduce_chairs()
+        self._set_state("PLAY")
+        self.next()
 
     def next(self):
         self._state = self.trigger_handler(self._state)
@@ -66,3 +83,16 @@ class Client():
 
     def get_players(self) -> dict[str, Player]:
         return self._players
+
+    def _set_state(self, state):
+        self._state = state
+
+    def _reduce_chairs(self):
+        self._current_chairs = self._current_chairs - 1
+
+    def _insert_input(self, player: Player, keypress):
+        # save dict of player keypresses
+        # only allow player to insert input if alive
+        if player.is_alive:
+            self._inputs[player] = keypress
+            return keypress
