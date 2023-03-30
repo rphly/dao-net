9616e5ced84bf74cc57651a953ab65434df0aa35
rphly <raphaelyeejunkai@gmail.com>
2023-03-30 13:42:34 +0800

    vejwve

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
     if not is_player_mode:
         host_port = host_port or 9999
         print(f"Starting host mode on port {host_port}.")
-        socket, tracker = Lobby().start(ip=host_ip,
+        socket, tracker = Lobby().start(host_ip,
                                         host_port=host_port or 9999, player_name=player_name)
     else:
         print("Starting in player mode.")
